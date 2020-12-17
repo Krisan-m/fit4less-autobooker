adb7aa95f414daf992675e1134d3ee6c3d886d4f
krisan-m <km.krisan@gmail.com>
2020-12-16 21:40:22 -0500

    Update

--- autobooker.py
+++ autobooker.py
@@ -53,17 +53,16 @@ try:
     print("Logged In!")
 
     # Find your club
-#     if "F4L_CLUB" in os.environ:
-#         driver.find_element_by_id("btn_club_select").click()
-#         driver.implicitly_wait(3)
-#         all_clubs = driver.find_element_by_id("modal_clubs").find_element_by_class_name("dialog-content").find_elements_by_class_name("button")
-#         for club in all_clubs:
-#             if os.getenv("F4L_CLUB") == club.text:
-#                 print("Club found: ", club.text)
-#                 club.click()
-#                 break
-    
-    driver.implicitly_wait(5)
+    if "F4L_CLUB" in os.environ:
+        driver.find_element_by_id("btn_club_select").click()
+        driver.implicitly_wait(3)
+        all_clubs = driver.find_element_by_id("modal_clubs").find_element_by_class_name("dialog-content").find_elements_by_class_name("button")
+        for club in all_clubs:
+            if os.getenv("F4L_CLUB") == club.text:
+                print("Club found: ", club.text)
+                club.click()
+                break
+        driver.implicitly_wait(5)
 
     curr_dt = datetime.now(timezone('est'))
     curr_time = datetime.strptime(str(curr_dt.hour) + ":" + str(curr_dt.minute), '%H:%M')   
@@ -77,11 +76,12 @@ try:
         driver.find_element_by_id("date_" + str(booking_date)).click()
         driver.implicitly_wait(5)
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        driver.implicitly_wait(3)
+        driver.implicitly_wait(1)
 
         # Check current reservations
         already_have_reservation = False
         try:            
+            driver.implicitly_wait(0.5)
             reserved_slots = driver.find_element_by_class_name("reserved-slots").find_elements_by_class_name("time-slot-box")      
             for slot in reserved_slots:                
                 if str(curr_dt.day + i) == str(slot.text).split()[4]:
